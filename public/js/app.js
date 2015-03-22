80636fb96576d17ff26dc3837354428666dc3cd7
iansun <lauer3912@gmail.com>
2015-03-22 16:15:21 +0800

    add some files

---
+++ public/js/app.js
@@ -0,0 +1,508 @@
+/**
+ * Created by Ian on 2015/3/5.
+ */
+
+(function () {
+    window['UI'] = window['UI'] || {};
+    window.UI.c$ = window.UI.c$ || {};
+})();
+
+(function () {
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    var c$ = {};
+    var b$ = BS.b$;
+    var $fc = FilesCacheModule;
+    c$ = $.extend(window.UI.c$, {});
+
+    // 初始化标题及版本
+    c$.initTitleAndVersion = function(){
+        document.title = b$.App.getAppName();
+    };
+
+
+    // 设置UI部分与逻辑交互
+    c$.setupUI = function(){
+        "use strict";
+
+        // UI 的Actions
+        c$.UIActions = {
+            buyPlugin:function(id){
+                alert(id);
+                var $iap = IAPModule;
+                $iap.buyProduct({
+                    productIdentifier:id,
+                    quantity:1
+                })
+            }
+
+            ,loadFile:function(id){
+
+                $fc.reLoadFile(id, function(obj){
+                    alert('loadFile')
+                })
+            }
+            ,saveFile:function(id){
+                alert('saveFile')
+            }
+            ,removeFilesItem:function(id){
+                alert('removeFilesItem')
+            }
+            ,createNew:function(){
+                alert('crateItem')
+            }
+            ,importFiles:function(){
+                alert('importFiles')
+            }
+        };
+
+
+        // 配置编辑器
+        function config_editor(){
+            //配置emoji的. 配置 You can custom Emoji's graphics files url path
+            editormd.emoji = {
+                path  : "http://www.emoji-cheat-sheet.com/graphics/emojis/",
+                ext   : ".png"
+            };
+
+            //配置Twemoji的. Twitter Emoji (Twemoji)  graphics files url path
+            editormd.twemoji = {
+                path : "http://twemoji.maxcdn.com/72x72/",
+                ext  : ".png"
+            };
+
+            //配置@link 的base url前缀
+            editormd.urls.atLinkBase = "https://github.com/";
+
+            //配置
+            var default_toolbarIcons = function () {
+                return ["undo", "redo", "|",
+                    "bold","del", "italic", "quote", "|",
+                    "h1", "h2", "h4", "h4", "h5", "h6", "|",
+                    "list-ul", "list-ol", "hr", "|",
+                    "link", "anchor", "image", "code", "|",
+                    "preview", "watch", "|",
+                    "clear"]
+            };
+
+            //插件
+            c$.ui_ele_editor = editormd("test-editormd", {
+                width: "100%",
+                height: "720",
+                path: 'common/editor.md/1.3/editor.md/lib/'
+                //toolbarIcons: default_toolbarIcons()
+            });
+
+            //工具栏自动固定定位的开启与禁用
+            c$.ui_ele_editor.config("toolbarAutoFixed", true);
+
+            //设置自动高度处理
+            c$.ui_ele_editor.config("autoHeight", true);
+
+            //激活Github Flavored Markdown task lists
+            c$.ui_ele_editor.config("taskList", true);
+
+            //激活emoji表情功能
+            c$.ui_ele_editor.config("emoji", true);
+
+            //激活atLink
+            c$.ui_ele_editor.config("atLink", true);
+
+            //激活emailLink
+            c$.ui_ele_editor.config("emailLink", true);
+
+            //激活flowChart
+            c$.ui_ele_editor.config("flowChart", true);
+
+            //激活sequenceDiagram
+            c$.ui_ele_editor.config("sequenceDiagram", true);
+
+            //激活科学公式 Tex
+            c$.ui_ele_editor.config("tex", true);
+
+            //激活Toc(目录功能)
+            c$.ui_ele_editor.config("toc", true);
+
+            //激活代码折叠功能
+            c$.ui_ele_editor.config("codeFold", true);
+
+            //激活htmlDecode.开启HTML标签解析，为了安全性，默认不开启
+            c$.ui_ele_editor.config("htmlDecode", true);
+
+            //高亮显示当前行
+            c$.ui_ele_editor.config("styleActiveLine", true);
+
+            //显示行号
+            c$.ui_ele_editor.config("lineNumbers", true);
+
+            //激活只读模式
+            c$.ui_ele_editor.config("readOnly", true);
+
+            //激活实时预览
+            c$.ui_ele_editor.config("watch", true);
+
+            //激活搜索替换功能
+            c$.ui_ele_editor.config("searchReplace", true);
+
+            //激活控制键盘快捷键的映射(禁用某些快捷键的功能)
+            c$.ui_ele_editor.config("disabledKeyMaps",[]);
+        }
+    };
+
+    // 配置路由控制
+    c$.configRoute = function(){
+        "use strict";
+
+        if (typeof Router === "undefined"){console.error('director router not config...');return;}
+
+        var fn_showOrHide = function(eleList, show, auto){
+            $.each(eleList, function(index, ele){
+                if(show){
+                    if($(ele).is(":visible")==false){$(ele).show()}
+                }else{
+                    if($(ele).is(":visible")==true)$(ele).hide();
+                }
+
+            });
+        };
+
+
+        var leftNav = function(){
+            console.log("left nav");
+            var ele = $('#leftNav');
+            if($.trim(ele.html()).length == 0){
+                var o = {
+                    appName: "MarkdownD",
+                    navList: [
+                        {name: "Files", href: "#/files", class:" icon-tab"}
+                        ,{name: "Workspace", href: "#/workspace", class:" icon-dashboard"}
+                        ,{name: "Plugins", href: "#/pluginsMgr", class:" icon-extension"}
+                        ,{name: "Settings", href: "#/settings", class:" icon-settings"}
+                        ,{name: "Help", href: "#/help", class:" icon-help"}
+
+                    ]
+                };
+
+                var html = template('tpl_leftNav', o);
+                ele.html(html);
+
+            }
+
+            fn_showOrHide(['#leftNav'], true);
+        };
+
+        var files = function(){
+            console.log("files");
+
+
+            var o = {
+                files: $fc.getAllFiles()
+            };
+
+            var ele = $('#view-files');
+            var html = template('tpl_files', o);
+            ele.html(html);
+
+            fn_showOrHide(['#leftNav','#view-workspace','#view-plugins', '#view-settings', '#view-help'], false);
+            fn_showOrHide(['#view-files'], true);
+        };
+
+        var workspace = function(){
+            console.log("workspace");
+
+            var ele = $('#view-workspace');
+            var html = template('tpl_workspace', {});
+            ele.html(html);
+
+            fn_showOrHide(['#leftNav','#view-files','#view-plugins', '#view-settings', '#view-help'], false);
+            fn_showOrHide(['#view-workspace'], true);
+        };
+
+        var settings = function(){
+            console.log("settings");
+
+            fn_showOrHide(['#leftNav','#view-files','#view-plugins', '#view-workspace', '#view-help'], false);
+            fn_showOrHide(['#view-settings'], true);
+        };
+
+        var pluginsMgr = function(){
+            console.log("pluginsMgr");
+            var ele = $('#view-plugins');
+
+
+            //从插件系统中，获取并整理
+            var $iap = IAPModule;
+            var enablePlugins = $iap.getAllEnablePlugins();
+
+
+
+            var o = {
+                plugins:enablePlugins
+            };
+
+            var html = template('tpl_pluginsMgr', o);
+            ele.html(html);
+
+            fn_showOrHide(['#leftNav', '#view-files','#view-workspace','#view-settings', '#view-help'], false);
+            fn_showOrHide(['#view-plugins'], true);
+        };
+
+        var help = function(){
+            console.log("help");
+
+            fn_showOrHide(['#leftNav','#view-files', '#view-plugins', '#view-workspace', '#view-settings'], false);
+            fn_showOrHide(['#view-help'], true);
+        };
+
+        var myRoutes = {
+            "/leftNav":leftNav,
+            '/files':files,
+            '/workspace':workspace,
+            '/settings' : settings,
+            '/pluginsMgr': pluginsMgr,
+            '/help':help
+
+        };
+
+        var router = Router(myRoutes);
+        router.init();
+
+
+
+    };
+
+    // 绑定系统Preferences菜单
+    c$.bindSystemPreferencesMenu = function(cb){
+        "use strict";
+        var cbName = b$._get_callback(function(info){
+            cb && cb(info)
+        }, true);
+
+        if(b$.pN){
+            var obj = JSON.stringify({menuTag:903, action:cbName});
+            b$.SystemMenus.setMenuProperty(obj);
+        }
+    };
+
+    // 绑定插件引导及相关的插件
+    c$.startPluginEngine = function(cb){
+        "use strict";
+
+        c$.plugin_callbacks = $.Callbacks(); // 注册业务逻辑回调(使用Jquery的Callbacks())
+        c$.plugin_callbacks.add(cb || function(obj){
+
+        });
+
+        var cbName = b$._get_callback(function(obj){
+            console.log($.obj2string(obj));
+            // 声明处理插件初始化的方法
+            function process_init(obj){
+                var c$ = UI.c$;
+                var b$ = BS.b$;
+                try{
+                    if (obj.type == "type_initcoresuccess") {
+
+                    }else if(obj.type == "type_initcorefailed") {
+                        console.error('init core plugin failed!');
+                    }
+                }catch(e){
+                    console.error(e);
+                }
+
+            }
+
+            // 声明处理CLI的回调处理
+            function process_dylibCLI(obj){
+                var c$ = UI.c$;
+                var b$ = BS.b$;
+
+                try{
+                    var infoType = obj.type;
+                    if (infoType == 'type_clicall_start'){
+
+                    }else if(infoType == 'type_clicall_reportprogress'){
+
+                    }else if(infoType == 'type_clicall_end'){
+
+                    }
+
+                }catch(e){
+                    console.error(e);
+                }
+            }
+
+            // 声明处理ExecCommand的方法
+            function process_execCommand(obj){
+                var c$ = UI.c$;
+                var b$ = BS.b$;
+
+                try{
+                    var infoType = obj.type;
+                    if(infoType == 'type_addexeccommandqueue_success'){
+                        var queueID = obj.queueInfo.id;
+                        b$.sendQueueEvent(queueID, "execcommand", "start");
+                    } else if(infoType == 'type_execcommandstart'){
+
+                    } else if(infoType == 'type_reportexeccommandprogress'){
+
+                    } else if(infoType == 'type_execcommandsuccess'){
+
+                    } else if(infoType == 'type_canceledexeccommand'){
+
+                    } else if(infoType == 'type_execcommanderror'){
+
+                    }
+                }catch(e){
+                    console.error(e);
+                }
+
+            }
+
+            // 声明处理Task的方法
+            function process_task(obj){
+
+                var c$ = UI.c$;
+                var b$ = BS.b$;
+                try{
+                    var infoType = obj.type;
+                    if(infoType == "type_addcalltaskqueue_success"){
+                        var queueID = obj.queueInfo.id;
+                        b$.sendQueueEvent(queueID, "calltask", "start");
+
+                        c$.plugin_callbacks.fire({type:'_native_task_added', data:obj});
+                    }else if(infoType == "type_calltask_start"){
+                        var queueID = obj.queueInfo.id;
+                        c$.plugin_callbacks.fire({type:'_native_task_started', data:obj});
+
+                    }else if(infoType == "type_calltask_error"){
+                        console.error($.obj2string(obj));
+                        c$.plugin_callbacks.fire({type:'_native_task_error', data:obj});
+
+                    }else if(infoType == "type_calltask_success"){
+                        console.log($.obj2string(obj));
+                        c$.plugin_callbacks.fire({type:'_native_task_finished', data:obj});
+
+                    }else if(infoType == "type_type_calltask_cancel"){
+                        console.log($.obj2string(obj));
+                        c$.plugin_callbacks.fire({type:'_native_task_canceled', data:obj});
+                    }
+                }catch(e){
+                    console.error(e);
+                }
+
+            }
+
+            // 以下是调用顺序
+            process_init(obj);
+            process_dylibCLI(obj);
+            process_execCommand(obj);
+            process_task(obj);
+
+        }, true);
+
+        if(b$.pN){
+            b$.enablePluginCore([], cbName);
+        }
+
+    };
+
+    // 处理IAP
+    c$.configIAP = function(cb){
+        "use strict";
+
+        if(typeof IAPModule == 'undefined'){return console.error('IAPModule is undefined.')}
+
+        var prefix = b$.App.getAppId() + ".plugin.", defaultImg = "images/linearicons.png";
+        var pluginsData = [
+            {enable:true, inAppStore: false, id:prefix + "support.importFile", type:"", quantity:1, price:"1$", name:"Open File", description: "支持导入文件", img:"images/linearicons.png"}
+            ,{enable:true, inAppStore: false, id:prefix + "support.dragFile", type:"", quantity:1, price:"1$", name:"Drag File", description: "支持拖拽文件", img:defaultImg}
+            ,{enable:true, inAppStore: false, id:prefix + "support.fileSave", type:"", quantity:1, price:"2$", name:"File Save", description: "支持保存文件功能", img:defaultImg}
+            ,{enable:true, inAppStore: true, id:prefix + "support.dirTree", type:"", quantity:0, price:"3$", name:"File Directory", description: "支持目录树功能", img:defaultImg}
+            ,{enable:true, inAppStore: true, id:prefix + "support.taskList", type:"", quantity:0, price:"2$", name:"Support TaskList", description: "支持Github task lists", img:defaultImg}
+            ,{enable:true, inAppStore: true, id:prefix + "support.emoji", type:"", quantity:0, price:"3$", name:"Support Emoji", description: "支持emoji表情功能", img:defaultImg}
+            ,{enable:true, inAppStore: true, id:prefix + "support.atLink", type:"", quantity:0, price:"1$", name:"Support atLink", description: "支持atLink功能", img:defaultImg}
+            ,{enable:true, inAppStore: true, id:prefix + "support.emailLink", type:"", quantity:0, price:"1$", name:"Support emailLink", description: "支持emailLink功能", img:defaultImg}
+            ,{enable:true, inAppStore: true, id:prefix + "support.flowChart", type:"", quantity:0, price:"2$", name:"Support FlowChart", description: "支持flowChart功能", img:defaultImg}
+            ,{enable:true, inAppStore: true, id:prefix + "support.sequenceDiagram", type:"", quantity:0, price:"2$", name:"Support SequenceDiagram", description: "支持sequenceDiagram功能", img:defaultImg}
+            ,{enable:true, inAppStore: true, id:prefix + "support.tex", type:"", quantity:0, price:"2$", name:"Support Tex", description: "支持tex功能", img:defaultImg}
+            ,{enable:true, inAppStore: true, id:prefix + "support.toc", type:"", quantity:0, price:"2$", name:"Support Toc", description: "支持toc功能", img:defaultImg}
+            ,{enable:true, inAppStore: true, id:prefix + "support.codeFold", type:"", quantity:0, price:"1$", name:"Support CodeFold", description: "支持codeFold功能", img:defaultImg}
+            ,{enable:true, inAppStore: true, id:prefix + "support.htmlDecode", type:"", quantity:0, price:"1$", name:"Support HTMLDecode", description: "支持htmlDecode功能", img:defaultImg}
+            ,{enable:true, inAppStore: true, id:prefix + "support.styleActiveLine", type:"", quantity:0, price:"1$", name:"Support Style Active Line", description: "支持styleActiveLine功能", img:defaultImg}
+            ,{enable:true, inAppStore: true, id:prefix + "support.lineNumbers", type:"", quantity:0, price:"1$", name:"Support Line Numbers", description: "支持lineNumbers功能", img:defaultImg}
+            ,{enable:true, inAppStore: true, id:prefix + "support.readOnly", type:"", quantity:0, price:"1$", name:"Support ReadOnly", description: "支持readOnly功能", img:defaultImg}
+            ,{enable:true, inAppStore: true, id:prefix + "support.searchReplace", type:"", quantity:0, price:"1$", name:"Support Search Replace", description: "支持searchReplace功能", img:defaultImg}
+        ];
+
+        var $iap = IAPModule;
+
+        $iap.init(pluginsData);
+
+        // IAP的回调函数
+        var fnName = b$._get_callback(function(obj){
+            try{
+                var info = obj.info, notifyType = obj.notifyType;
+
+                if(notifyType == "ProductBuyFailed"){
+                    //@"{'productIdentifier':'%@', 'message':'No products found in apple store'}"
+                    var pluginId = info.productIdentifier;
+                    var message = info.message;
+
+                    var log = $.stringFormat("{0} order plugin failed! {1}", pluginId, message);
+                    console.warn(log);
+                }else if(notifyType == "ProductPurchased"){
+                    //@"{'productIdentifier':'%@', 'quantity':'%@'}"
+                    var pluginId = info.productIdentifier;
+                    $iap.syncDataWithAppStore(pluginId);
+
+                    var log = $.stringFormat("{0} order plugin success!", pluginId);
+                    console.log(log);
+                }else if(notifyType == "ProductPurchaseFailed"){
+                    //@"{‘transactionId':'%@',‘transactionDate’:'%@', 'payment':{'productIdentifier':'%@','quantity':'%@'}}"
+                    var pluginId = info.payment.productIdentifier;
+                    var orderDate = info.transactionDate;
+
+                    var log = $.stringFormat("{0} order plugin failed! orderDate {1}", pluginId, orderDate);
+                    console.log(log);
+                }else if(notifyType == "ProductPurchaseFailedDetail"){
+                    //@"{'failBy':'cancel', 'transactionId':'%@', 'message':'%@', ‘transactionDate’:'%@', 'payment':{'productIdentifier':'%@','quantity':'%@'}}"
+                    var pluginId = info.payment.productIdentifier;
+
+                    var log = $.stringFormat("error: {0} failed by {1} ({2}) order date: {3}", pluginId, info.failBy, info.message, info.transactionDate);
+                    console.log(log);
+                }else if(notifyType == "ProductRequested"){
+                    var productInfoList = info;
+                    if(typeof info == "string"){
+                        productInfoList = JSON.parse(info);
+                    }
+
+                    var log = $.stringFormat("Request product info from app store.");
+                    console.log(log);
+
+                    $iap.updatePluginsDataWithList(productInfoList);
+                }else if(notifyType == "ProductCompletePurchased"){
+                    //@"{'productIdentifier':'%@', 'transactionId':'%@', 'receipt':'%@'}"
+                    var pluginId = info.productIdentifier;
+                    var log = $.stringFormat("pluginId: {0}, transactionId: {1}, receipt: {2}", pluginId, info.transactionId, info.receipt);
+                    console.log(log);
+                }
+
+
+            }catch(e){console.error(e)}
+        }, true);
+
+        // 开启IAP
+        b$.IAP.enableIAP({cb_IAP_js: fnName, productIds: $iap.getEnableInAppStorePluginIDs()});
+    };
+
+    // 启动
+    c$.launch = function(){
+        "use strict";
+        c$.initTitleAndVersion();
+        c$.configIAP();
+        c$.configRoute();
+        c$.setupUI();
+    };
+
+
+
+}());
---
+++ public/js/files_cache.js
@@ -0,0 +1,193 @@
+/**
+ * Created by Ian on 2015/3/22.
+ */
+;(function(factory){
+    "use strict";
+
+    // CommonJS/Node.js
+    if (typeof require === "function" && typeof exports === "object" && typeof module === "object")
+    {
+        module.exports = factory;
+    }
+    else if (typeof define === "function")  // AMD/CMD/Sea.js
+    {
+        if (define.amd) // for Require.js
+        {
+            /* Require.js define replace */
+        }
+        else
+        {
+            define(["jquery"], factory);  // for Sea.js
+        }
+    }
+    else
+    {
+        window.FilesCacheModule = factory();
+    }
+
+
+}(function () {
+    var check$ = true;
+    if(check$){
+        if (typeof ($) === "undefined") {
+            console.error("no jquery is loaded...");
+            return ;
+        }
+    }
+
+    var fc = {};
+
+    fc.data = [
+        {
+            id:'id001',        // 唯一标识
+            name:'welcome.md', // 别名
+            is_tmp:false,      // 是否是临时文件
+            path:'/usr/local/1.md', // 路径
+            ext:'md',          // 扩展名
+            content:''         // 内容。base64编码
+        }
+    ];
+
+    fc.getAllFiles = function(){
+        "use strict";
+        var t = this;
+        return t.data;
+    };
+
+    /**
+     * 创建新文件
+     * @param name 新文件的名称
+     * @param useTmpFile 是否使用tmp文件
+     * @param cb 回调函数
+     */
+    fc.createNewFile = function(name = 'untitled', useTmpFile = false, cb = null){
+        "use strict";
+        var t = this;
+
+        var fileObj = {
+            id: Date.now().toString(),
+            name: name,
+            is_tmp : useTmpFile,
+            path: "",
+            ext: "md",
+            content:""
+        };
+
+        t.data.push(fileObj);
+        cb && cb(fileObj);
+
+        return fileObj;
+    };
+
+    /**
+     * 添加文件到缓存
+     * @param params 文件对象
+     * @param cb 回调处理
+     */
+    fc.addFile = function(params, cb){
+        "use strict";
+        var t = this;
+
+        $.each(t.data, function(index, obj){
+            try{
+                if(obj.path == params.path){
+                    cb && cb(false);
+                    return false;
+                }
+            }catch(e){console.error(e)}
+        });
+
+
+        var fileObj = {
+            id: Date.now().toString(),
+            name: params.name || 'untitled',
+            is_tmp:false,
+            path:params.path,
+            ext:params.ext || 'md',
+            content:params.content
+        };
+
+        t.data.push(fileObj);
+        cb && cb(fileObj);
+
+    };
+
+    /**
+     * 重新加载文件
+     * @param id 文件id
+     * @param cb 回调处理
+     */
+    fc.reLoadFile = function(id, cb){
+        "use strict";
+        var t = this;
+
+        $.each(t.data, function(index, obj){
+            if(obj.id == id){
+                cb && cb(obj);
+                return false;
+            }
+        })
+
+    };
+
+    /**
+     * 保存文件
+     * @param id 文件id
+     * @param cb 回调处理
+     */
+    fc.saveFile = function(id, cb){
+        "use strict";
+        var t = this;
+
+        $.each(t.data, function(index, obj){
+            if(obj.id == id){
+                cb && cb(obj);
+                return false;
+            }
+        })
+    };
+
+    /**
+     * 删除文件缓存项
+     * @param id
+     * @param cb
+     */
+    fc.removeFile = function(id, cb){
+        "use strict";
+        var t = this;
+
+        $.each(t.data, function(index, obj){
+            if(obj.id == id){
+                cb && cb(obj);
+                return false;
+            }
+        })
+    };
+
+    /**
+     * 查找文件
+     * @param id id
+     * @param cb
+     * @returns {boolean}
+     */
+    fc.findFile = function(id, cb){
+        "use strict";
+        var t = this;
+
+        var find = false;
+        $.each(t.data, function(index, obj){
+            if(obj.id == id){
+                cb && cb(obj);
+                find = true;
+                return false;
+            }
+        });
+
+        return find;
+    };
+
+
+
+
+    return fc;
+}));
